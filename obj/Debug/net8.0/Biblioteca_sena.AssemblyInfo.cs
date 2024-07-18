2a8390e66cbd5a7899dcb5b43f96b74e191ccad7
Deilons <apolo4748@gmail.com>
2024-07-18 11:44:14 -0500

    feat: cration of book class

--- obj/Debug/net8.0/Biblioteca_sena.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca_sena.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca_sena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbdecf85cfdd6ad0d024e09037a6de4ca09deb64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86290e3ef92c1c492e050e1f9db281567cb59f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca_sena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca_sena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
